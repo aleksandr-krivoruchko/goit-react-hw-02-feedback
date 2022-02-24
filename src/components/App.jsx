ae90e9a3a52527404e139d205ea97dd5c97de760
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-02-24 09:14:11 +0200

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,23 @@ import { Section } from "components/Section/Section";
 import { FeedbackOptions } from "components/FeedbackOptions/FeedbackOptions";
 import { Statistics } from "components/Statistics/Statistics";
 
+import state from "../components/state.json";
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
+        fontSize: 30,
         textTransform: 'uppercase',
         color: '#010101',
       }}
     >
      <Section title="please leave feedback">
-	  {/* <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions> */}
+	  <FeedbackOptions ></FeedbackOptions>
 	  </Section>
 	       <Section title="statistics">
-		  {/* <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics> */}
+		  <Statistics good={state.good} neutral={state.neutral} bad={state.bad} total={state.good+state.neutral+state.bad} positivePercentage={Math.round(state.good/(state.good+state.neutral+state.bad)*100)}></Statistics>
 	  </Section>
 
 
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,5 +2,9 @@ import PropTypes from 'prop-types';
 
 
 export function FeedbackOptions(options, onLeaveFeedback) {
-	
+   return (<>
+      <button type="button" className='btn btn--good'>Good</button>
+         <button type="button" className='btn btn--neutral'>Neutral</button>
+         <button type="button" className='btn btn--bad'>Bad</button>
+   </>);
 }
